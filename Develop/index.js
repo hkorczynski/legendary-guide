ad173b081e161776eb124c4ab925af49ab234eb1
Heather Korczynski <hk@Heathers-MBP.attlocal.net>
2022-11-11 23:14:22 -0600

    started variables and questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const utils = require ("utils");
+
+const generatorMarkdown = required('./utils/generatorMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input";
+    message: "What is the title of the project?",
+    name: "Title";
+}, {
+    type: "input",
+    message: "What is the project about"
+}
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
